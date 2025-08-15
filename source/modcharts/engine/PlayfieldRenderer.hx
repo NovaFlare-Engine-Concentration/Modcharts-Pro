08881d1b7d61862d94ffd461ba920288cc68bb4b
Ajwwkxkdang <107187521+Ajwyunsx@users.noreply.github.com>
2025-08-15 18:46:07 +0800

    Update PlayfieldRenderer.hx

--- source/modcharts/engine/PlayfieldRenderer.hx
+++ source/modcharts/engine/PlayfieldRenderer.hx
@@ -197,7 +197,7 @@ class PlayfieldRenderer extends FlxSprite //extending flxsprite just so i can ed
     {
         var noteDist = -0.45;
         if (ModchartUtil.getDownscroll(playStateInstance))
-            noteDist *= -0.95;
+            noteDist *= -1;
         return noteDist;
     }
 
