36610f071f364fa91fd8cc52e0088582c6cfa62d
Ajwwkxkdang <107187521+Ajwyunsx@users.noreply.github.com>
2025-08-15 15:48:32 +0800

    Update PlayfieldRenderer.hx

--- source/modcharts/engine/PlayfieldRenderer.hx
+++ source/modcharts/engine/PlayfieldRenderer.hx
@@ -73,7 +73,6 @@ class PlayfieldRenderer extends FlxSprite //extending flxsprite just so i can ed
         notes.visible = false;
 
         // //fix stupid crash because the renderer in playstate is still technically null at this point and its needed for json loading
-        instance.modManager = this;
         instance.modManager.renderer = this;
 
         tweenManager = new FlxTweenManager();
